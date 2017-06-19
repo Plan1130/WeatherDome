7662e108a85c475a985823570fb7e6494766e0cf
FlorianFFF <florianffff@gmail.com>
2017-06-19 12:05:01 +0200

    bug fix

--- WeatherDomeProject/src/weatherdomeproject/CSVReader.java
+++ WeatherDomeProject/src/weatherdomeproject/CSVReader.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package weatherdome;
+package weatherdomeproject;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
