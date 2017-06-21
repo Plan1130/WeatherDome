31caf94f67ce5f00c9ce89d636c83028608059a9
FlorianFFF <florianffff@gmail.com>
2017-06-21 11:24:09 +0200

    Beginnetje aan WeatherType

---
+++ WeatherDomeProject/src/weatherdomeproject/WeatherType.java
@@ -0,0 +1,34 @@
+/*
+ * WeatherDome!
+ */
+package weatherdomeproject;
+
+/**
+ *
+ * @author MAX 'imaal' G
+ */
+public class WeatherType {
+    private WeatherState state;
+    boolean isRaining;
+    rainIntensity rainType;
+    enum rainIntensity {LOW,MEDIUM,HIGH};
+    
+    public WeatherType(WeatherState state) {
+        this.state = state;
+        this.setValues();
+    }
+    
+    private void setValues() {
+        isRaining = state.getPercipitationAmount() > 1 ? true : false;
+        if (isRaining && state.getPercipitationAmount() < 10) {
+            rainType = rainIntensity.LOW;
+        } else if (state.getPercipitationAmount() > 10) {
+            rainType = rainIntensity.MEDIUM;
+        } else if (state.getPercipitationAmount() > 25) {
+            rainType = rainIntensity.HIGH;
+        }
+        
+        
+    }
+    
+}
